600e85c6745bdd51b8ac6fe27e9a8ff05ce172e2
abhaysarkar <abhaysheets@gmail.com>
2024-10-21 18:06:33 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
-    'http://yourdomain.com',
+    'https://boxcutting-frontend.vercel.app',
 ]
 
 
